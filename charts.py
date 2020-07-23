393effb753245b12ecde9987118ed33795975bb9
nickjevershed <nick.evershed@gmail.com>
2020-07-23 11:20:30 +1000

    reove plotly

--- charts.py
+++ charts.py
@@ -1,6 +1,3 @@
-import plotly.graph_objs as go
-import plotly.io as pio
-from plotly.offline import plot
 from getData import getData
 from yachtCharter import yachtCharter
 from syncDoc import syncDoc
