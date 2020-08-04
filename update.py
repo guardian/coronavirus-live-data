5d4c2262db820b1ccc1c3f45407cd58746b14cf1
jerseywonder <work@andyball.info>
2020-08-04 13:17:47 +1000

    Check for googledoc updates

---
+++ update.py
@@ -0,0 +1,43 @@
+import requests
+import pathlib
+import simplejson as json
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--key", "-k", help="set googledoc key")
+
+args = parser.parse_args()
+
+def load():
+	print ("File does not exist")
+	with open(key + '.json', 'w') as outfile:
+		json.dump(data, outfile)
+	trigger()
+
+def exist():
+	print ("File exist")
+	with open(key + '.json') as f:
+		json_data = json.load(f)
+		if sorted(json_data.items()) == sorted(data.items()):
+		    print("Same same")
+		else:
+		    print("Different")
+		    trigger()
+
+def trigger():		
+	if key == "1q5gdePANXci8enuiS4oHUJxcxC13d6bjMRSicakychE":
+		print ("run processData.py")
+		import processData
+
+if args.key == "1q5gdePANXci8enuiS4oHUJxcxC13d6bjMRSicakychE":
+	key = args.key
+	print(args.key)
+	data = requests.get('https://interactive.guim.co.uk/docsdata/' + key + '.json').json()['sheets']
+	local = pathlib.Path(key + '.json')
+	if local.exists ():
+	    exist()
+	else:
+	    load()
+else:
+	print("Nothing to see here")
